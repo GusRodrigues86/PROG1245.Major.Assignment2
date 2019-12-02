f3ac5819b0302aa90cb9aaf615f9920c4a2c2136
GusRodrigues <omykronbr@gmail.com>
2019-12-01 23:05:23 -0500

    i hate js

--- files/scripts/alert.js
+++ files/scripts/alert.js
@@ -1,3 +1,9 @@
+var form = document.getElementsByTagName('form');
+
 function submitAlert() {
-    alert("I am an alert box!");
+    if (form[0].checkValidity()) {
+        alert('Thank you for your feedback!');
+    } else if (form[1].checkValidity()) {
+        alert('Thank you for telling me more about the webpage');
+    }
 }
